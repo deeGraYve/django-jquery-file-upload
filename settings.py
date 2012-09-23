59a440bd3755cb7cfc9f95d001398c200851cdd8
Dmitry Golomidov <dmitry.golomidov@gmail.com>
2012-09-23 17:20:56 -0400

    fixing settings

--- settings.py
+++ settings.py
@@ -100,7 +100,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'django-jquery-file-upload.urls'
+ROOT_URLCONF = 'django_jquery_file_upload.urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
